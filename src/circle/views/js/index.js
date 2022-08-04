cfbfd39f974d82ef38646762a7766e755182cf34
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-08-04 14:50:05 -0300

    Drag circle in real time

---
+++ src/circle/views/js/index.js
@@ -0,0 +1,29 @@
+const socket = io()
+const circle = document.querySelector('#circle')
+
+document.addEventListener('mousedown', (e) => {
+	document.addEventListener('mousemove', drag)
+})
+
+document.addEventListener('mouseup', (e) => {
+	document.removeEventListener('mousemove', drag)
+})
+
+const drag = (e) => {
+	const position = {
+		y: e.clientY + 'px',
+		x: e.clientX + 'px',
+	}
+
+	drawCircle(position)
+	socket.emit('circle position', position)
+}
+
+const drawCircle = (position) => {
+	circle.style.top = position.y
+	circle.style.left = position.x
+}
+
+socket.on('move circle', (position) => {
+	drawCircle(position)
+})
